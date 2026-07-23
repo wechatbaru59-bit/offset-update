be33a8416ac210fd86aaf71eb867ba3778e66c83
wechatbaru59-bit <email_anda@contoh.com>
2026-07-23 21:52:30 +0800

    Upload versi Base64 penuh

---
+++ final_ultimate.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# =============================================================
+# SKRIP INI TELAH DIENCODE KEPADA BASE64 (RANTAI HURUF RAWAK)
+# =============================================================
+
+# Ambil dan decode rentetan Base64, terus jalankan
+echo "IyEvYmluL2Jhc2gKClBVUlBMRT0nXDAzM1swOzM1bScKWUVMTE9XPSdcMDMzWzA7MzNtJwpHUkVFTj0nXDAzM1swOzMybScKUkVEPSdcMDMzWzA7MzFtJwpDWUFOPSdcMDMzWzA7MzZtJwpXSElURT0nXDAzM1swOzk3bScKTkM9J1wwMzNbMG0nIA" | base64 -d | bash
